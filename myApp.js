39334543b81e98bc038cb43e81c3eeacb7cd08a4
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2022-09-12 00:11:15 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,13 +1,12 @@
 let express = require('express');
 let app = express();
 
-app.use(express.json())
 app.get('/json',(req,res)=>{
   var response = {message:"Hello json"};
   if(process.env.MESSAGE_STYLE === 'uppercase'){
     response.message=response.message.toUpperCase()
   }
-  res.json(response)
+  res.send(response)
 })
 
 
